d569f6d46432b155a456410f6947cab7a38fd6f9
WillQi <williamqipizza@gmail.com>
2018-09-15 13:27:42 -0400

    removes warning about using # and a element

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ export default class Navbar extends React.Component {
     return (
        <nav>
         <div className="nav-wrapper light-blue darken-4">
-          <a href="#" className="left brand-logo">Find My Team</a>
+          <Link to="/" className="left brand-logo">Find My Team</Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><Link to="/">Home</Link></li>
             <li><Link to="/login">Login</Link></li>
