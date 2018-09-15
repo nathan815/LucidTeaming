a88a55aed4b45b10da22ed6382aac5f6fb5e581b
Rishi Kothari <rishiosaur@gmail.com>
2018-09-15 11:10:21 -0400

    started components

---
+++ src/Components/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Headtext from "./text/Headtext"
+import Act from './ui/act'
+
+let Login = (props)=>{
+	return (
+		<div id="login">
+			<Headtext text="Login"/>
+		</div>
+	)
+}
+
+export default Login;
---
+++ src/Components/text/Headtext.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+var Headtext = (props) => {
+	return (
+		<h1>{props.text}</h1>
+	);
+}
+export default Headtext;
