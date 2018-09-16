67741235d80999258a8e6ab6d2e5396c5a9589dc
WillQi <williamqipizza@gmail.com>
2018-09-16 00:34:00 -0400

    project list

--- src/Components/MyProjects.js
+++ src/Components/MyProjects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import firebase from '../firebase';
 import "../css/MyProjects.css";
-import {Modal} from 'react-materialize';
+import {Modal, Button} from 'react-materialize';
 import { Link } from 'react-router-dom';
 import Project from './Project';
 
@@ -15,7 +15,7 @@ export default class MyProjects extends React.Component {
 
         firebase.database.collection("projects").get().then(({docs}) => {
             this.setState({
-                projects: docs.map(doc => doc.data())
+                projects: docs.map(doc => {return {data: doc.data(), id: doc.id}})
             });
         }).catch(console.error);
 
@@ -25,23 +25,23 @@ export default class MyProjects extends React.Component {
         return (
                     <div className="card darken-1 left" id="projectsBar">
                         <div className="card-content">
-                            <span className="card-title">Projects</span>
+                            <span className="card-title">Your Projects</span>
                             {
                                 this.state.projects.length
                                 ?
-                                    this.state.projects.map(project => {
+                                    this.state.projects.map(({data: project, id}) => {
                                         return (
-                                            <Modal header={project.name} trigger={<Link to="#">{project.name}</Link>}>
-                                                <Project data={project}/>
-                                            </Modal>
+                                            <span>
+                                                <Link to={`/projects/${id}`}>{project.title || "Unnamed Project"}</Link>
+                                                <br />
+                                            </span>
                                         )
-                                        //return <Link to="/project" key={`${project.name}-${project.createdAt}`}>{project.name}</Link>
                                     })
                                 :
                                     <p>You don't have any projects!</p> //TODO: proper message or something.
                             }
                         </div>
-                        <div className="card-footer right">
+                        <div className="card-footer right" id="projectCreateFooter">
                             <Link to="/project">Create Project</Link>
                         </div>
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'react-materialize'
 import MyProjects from '../Components/MyProjects';
 import Welcome from '../Components/Welcome';
 
@@ -8,7 +6,6 @@ export default class Dashboard extends React.Component {
     render () {
         return (
         	<div>
-            	<Link to="/project"><Button>Make a new project</Button></Link> 
                 <MyProjects />
                 <Welcome />
             </div>
